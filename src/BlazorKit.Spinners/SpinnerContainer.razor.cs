bb68f328feb5ef34fb53cb86ac8048c4c34d5600
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2021-10-23 20:49:52 +0200

    refactoring and version.json

---
+++ src/BlazorKit.Spinners/SpinnerContainer.razor.cs
@@ -0,0 +1,31 @@
+﻿using BlazorComponentUtilities;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorKit.Spinners
+{
+    partial class SpinnerContainer
+    {
+        [Parameter] 
+        public RenderFragment ChildContent { get; set; }
+        [Parameter] 
+        public string BaseCss { get; set; }
+        
+        string CssClass =>
+            CssBuilder.Default(BaseCss)
+            .AddClass("sk-center", Center)
+            .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+        string Style =>
+            StyleBuilder.Empty()
+            .AddStyle("--sk-color", Color, when: !string.IsNullOrEmpty(Color))
+            .AddStyle("--sk-size", Size, when: !string.IsNullOrEmpty(Size))
+            .AddStyleFromAttributes(AdditionalAttributes)
+        .NullIfEmpty();
+    }
+}
